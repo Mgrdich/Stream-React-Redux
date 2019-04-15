4d874f467c6852bc05366e596495a60a46927a62
Mgrdich <46796009+Mgrdich@users.noreply.github.com>
2019-04-15 10:16:41 +0400

    Update myValidation.js

--- src/Validate/myValidation.js
+++ src/Validate/myValidation.js
@@ -1,26 +1,28 @@
-import Joi from 'joi-browser'
+import Joi from "joi-browser";
 export default function createValidator(schema) {
-    return values => {//i think this values is taken from the form redux
-        //console.log(Joi.validate(values, schema, { abortEarly: false }));//this will show exactly what is happening
-        const result = Joi.validate(values, schema, { abortEarly: false });
-        if (result.error === null) {
-            return {};
-        }
+  return values => {
+    //i think this values is taken from the form redux
+    //console.log(Joi.validate(values, schema, { abortEarly: false }));//this will show exactly what is happening
+    const result = Joi.validate(values, schema, { abortEarly: false });
+    if (result.error === null) {
+      return {};
+    }
 
-        const errors = result.error.details.reduce((all, cur) => {
-            const allErrors = Object.assign({}, all);//an alternative way is written in javascript reducer file
-            const path = cur.path[cur.path.length - 1];
-            const message = cur.message;
-            if (Object.prototype.hasOwnProperty.call(allErrors, path)) {//to check if allError object has a certian property
-                allErrors[path] += message;
-            } else {
-                allErrors[path] = message;
-            }
-            return allErrors;
-        }, {});
+    const errors = result.error.details.reduce((all, cur) => {
+      const allErrors = Object.assign({}, all); //an alternative way is written in javascript reducer file we can use spread operator here
+      const path = cur.path[cur.path.length - 1];
+      const message = cur.message;
+      if (Object.prototype.hasOwnProperty.call(allErrors, path)) {
+        //to check if allError object has a certian property
+        allErrors[path] += message; //error. its wring doing is here
+      } else {
+        allErrors[path] = message;
+      }
+      return allErrors;
+    }, {});
 
-        return errors;
-    };
+    return errors;
+  };
 }
 
 //than the redux form will take the error object with all of its properties
